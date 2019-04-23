436247d7ebc9a3e0609fe4088e8c20753be34247
Anthony Sun <auslei@gmail.com>
2019-04-23 20:22:40 +1000

    added pandas support with searchable tables

--- flask/FlaskApp/__init__.py
+++ flask/FlaskApp/__init__.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template
 import sys
+import pandas as pd
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return render_template("main.html")
+    #df = pd.DataFrame({"A": [1,2,3,4,5], "B": [6,7,8,9,10]})
+    df = pd.read_csv("./static/data/googleplaystore.csv").sample(30)
+    return render_template("main.html", data=df, columns=df.columns, datalen=len(df))
 
 @app.route('/dashboard')
 def dashboard():
-    return render_template("main.html")
+    df = pd.DataFrame({"A": [1,2,3,4,5], "B": [6,7,8,9,10]})
+    return render_template("dashboard.html", data=df, columns=df.columns, datalen=len(df))
 
 if __name__ == "__main__":
     app.run()
