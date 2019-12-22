4e3c8ff7617409310ba6c6f1d7f1960230d34eca
Anthony Sun <auslei@gmail.com>
2019-12-22 11:28:32 +1100

    put into functions

--- webscrapping/get_news.py
+++ webscrapping/get_news.py
@@ -1,6 +1,7 @@
 #%% Setup
 import urllib3
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 _base_url = "https://www.news.com.au/"
 _headers = {        
@@ -27,21 +28,30 @@ def get_content(url):
     return html
 
 #%% get all the new in the top news
-import re
-regex = re.compile("story-block.*")
-stories = get_content(_base_url).find_all('div', {'class': regex})
+def read_news(_base_url):
+    import re
+    regex = re.compile("story-block.*")    
 
-story_links = {"stories": [], "links": [], "content": []}
+    stories = {"timestamp": [], "headline": [], "link": [], "content": []}
 
-for story in stories:
-    heading = story.find('h4', {'class': 'heading'})
-    if heading:
-        a = heading.find('a')
-        story = a.text
-        link = a.get('href')
-        content = " ".join([p.text for p in get_content(link).find_all('p')])
-        story_links['stories'].append(story)
-        story_links['links'].append(link)
-        story_links['content'].append(content)
+    for story in  get_content(_base_url).find_all('div', {'class': regex}):
+        heading = story.find('h4', {'class': 'heading'})
+        if heading:
+            a = heading.find('a')
+            headline = a.text
+            link = a.get('href')
+            content = " ".join([p.text for p in get_content(link).find_all('p')])
+            stories['headline'].append(headline)
+            stories['link'].append(link)
+            stories['content'].append(content)
+            ts = story.find('span', {'class': 'datestamp'})
+            if ts:
+                stories['timestamp'].append(f"{ts.txt} {ts.find('span', {'class': 'time'}).text}")
+            else:
+                stories['timestamp'].append(datetime.now().strftime("Last updated %B %d, %Y %H:%M:%S AEDT"))
+            
+
+    return pd.DataFrame(stories)
 
 #%%
+df = read_news(_base_url)
